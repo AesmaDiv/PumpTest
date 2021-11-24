1ff860a9ccc1d208521b07cf5dcbb1d4d0cc7cb0
AesmaDiv <jhwh.0001@gmail.com>
2021-11-24 17:35:19 +0500

    Adding pyinstaller params

--- git_update.bat
+++ git_update.bat
@@ -1,5 +1,5 @@
 git config --global user.name "AesmaDiv"
 git config --global user.email "jhwh.0001@gmail.com"
 git add .
-git commit -m "готов к симуляционной проверке (значения датчиков - слайдерами)"
+git commit -m "Adding pyinstaller params"
 git push origin main
